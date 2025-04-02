b87c8cf796136a424d587e9b89d5cf756455892e
bristysaha <bristysaha046@gmail.com>
2025-04-03 00:17:20 +0530

    Add files via upload

---
+++ DAY 01/addition.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () 
+{
+  int a = 50, b = 20;
+  int c = a + b;
+  printf( "The addition of two numbers\n");
+  printf("1.The value of A is:%d\n",a);
+  printf("2.The Value of B is:%d\n",b );
+  printf("3.The Sum of A+B is:%d\n",c);
+  return 0;
+}
---
+++ DAY 01/substraction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () 
+{
+  int a = 50, b = 20;
+  int c = a - b;
+  printf( "The substraction of two numbers\n");
+  printf("1.The value of A is:%d\n",a);
+  printf("2.The Value of B is:%d\n",b );
+  printf("3.The Difference of A-B is:%d\n",c);
+  return 0;
+}
