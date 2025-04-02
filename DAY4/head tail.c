ab8b2f54ed98262ef3b3de7e96e4c9b682f68ca5
bristysaha <bristysaha046@gmail.com>
2025-04-03 00:37:47 +0530

    Add files via upload

---
+++ DAY4/head tail.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+char a;
+printf ("Enter H or T in Upper Cases:");
+scanf("%C",&a);
+if (a=='T')
+{
+	printf("Tail");
+} else
+{
+	printf("Head");
+	
+}
+return 0;
+}
+
+
+
+  
---
+++ DAY4/roll 100.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	float r;
+	printf ("enter the roll:");
+	scanf ("%f",&r);
+	if (r==100)
+	{
+		printf ("present");
+    }
+	return 0;
+}
+
